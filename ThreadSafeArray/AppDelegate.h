71a5856b4cd07c5fa6bc1f70ae9583bc06d4540e
Hungju Lu <jefflen@gmail.com>
2016-12-06 13:28:16 +0000

    Initial Commit

---
+++ ThreadSafeArray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadSafeArray
+//
+//  Created by Hungju Lu on 06/12/2016.
+//  Copyright © 2016 Hungju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
